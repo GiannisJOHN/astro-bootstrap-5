797accaae7c8756d1294c9818a6d76f894c3e357
Ioannis Mentesidis <51725514+GiannisJOHN@users.noreply.github.com>
2023-09-10 05:07:55 +0200

    initial

---
+++ src/preact.jsx
@@ -0,0 +1,8 @@
+import { h } from 'preact';
+import Template from './template-basic/template';
+
+function App() {
+    return <Template />
+}
+
+export default App
---
+++ src/template-basic/template/index.jsx
@@ -0,0 +1,13 @@
+import { h } from 'preact';
+import Navbar from '../components/navbar';
+
+function Template() {
+
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Template
